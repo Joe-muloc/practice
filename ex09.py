2ae570d7109385c9871b5a7a9dd420e55c1f10be
Joe-muloc <jyl19981211@gmail.com>
2020-01-08 05:41:37 +0000

    ex9

--- ex09.py
+++ ex09.py
@@ -9,4 +9,11 @@ def processingSingleImage(image):
   pt1 = (width * 0.45, height * 0.65)
   pt2 = (width * 0.55, height * 0.65)
   pt3 = (width, height * 1.0)
-  pt3 = (0, height * 1.0)
+  pt4 = (0, height * 1.0)
+
+  roi_coners = np.array([[pt1, pt2, pt3, pt4]], dtype=np.int32)
+
+  result = polyROI(result, roi_corners)
+  lines = houghLinesP(result, 1, np.pi/180, 40)
+  result = drawHoughLinesP(image, lines)
+  return results
